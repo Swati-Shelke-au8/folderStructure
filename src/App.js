561e0a2da2ef360fee56098c88464077ee519dc4
Swati Shelke <ghogareswati14@gmail.com>
2024-07-16 12:03:43 +0530

    folder structure management

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { FolderProvider } from "./FolderContext";
-// import FolderTree from "./FolderTree";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <FolderProvider>
-//       <div className="App">
-//         <h1>Folder Structure Manager</h1>
-//         <FolderTree />
-//       </div>
-//     </FolderProvider>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import FolderTree from "./components/FolderTree";
 import "./App.css";
